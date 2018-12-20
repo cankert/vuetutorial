70aa791b939865a14dbb559664e4d280cd2d0522
cankert <ankert.c@gmail.com>
2018-12-20 12:00:38 +0100

    Finish lesson

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 new Vue({
-	el: '#app',
-	data: {
-		title: 'Becoming a Vue Ninja',
-		items: ['Mushroom', 'Green Shells', 'Red Shells', 'Star']
-	},
-	methods: {
-			
-	}
-})
+  el: "#app",
+  data: {
+    title: "Becoming a Vue Ninja",
+    items: ["Mushroom", "Green Shells", "Red Shells", "Star"],
+    ninjas: [
+      { name: "Crystal", age: 25, belt: "Black" },
+      { name: "Ryu", age: 30, belt: "Red" },
+      { name: "Ken", age: 20, belt: "White" }
+    ]
+  },
+  methods: {}
+});
